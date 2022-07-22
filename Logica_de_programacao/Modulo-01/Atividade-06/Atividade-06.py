91f7fdc9915be5a6b34e771b369169545db28f2c
dayvisonfaierstein <108841900+dayvisonfaierstein@users.noreply.github.com>
2022-07-22 19:25:01 -0300

    resolução

---
+++ Logica_de_programacao/Modulo-01/Atividade-06/Atividade-06.py
@@ -0,0 +1,21 @@
+#Declarando as variáveis do programa
+maiorNotaA = 0.0
+maiorNotaB = 0.0
+maiorNotaC = 0.0
+maiorNotaD = 0.0
+maiorNotaGeral = 0.0
+
+maiorNotaA = ""
+maiorNotaB = ""
+maiorNotaC = ""
+maiorNotaD = ""
+maiorNotaGeral = ""
+
+qtdAprovadosA = 0
+qtdAprovadosB = 0
+qtdAprovadosC = 0
+qtdAprovadosD = 0
+
+print ("programa para cadastro de notas do vestibular. Insira notas destindas por turma")
+#Laço de repetição para pegar as notas dos alunos de cada turma - Turma A
+for i in range(5)
